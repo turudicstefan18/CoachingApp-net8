5d860b2b779ade0d7f96ef76a92eb80c5cb01849
Stefan <turudicstefan@gmail.com>
2024-09-10 02:45:25 +0200

    end of section 8 - automapper

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -16,7 +16,9 @@ public class AccountController(DataContext context, ITokenService tokenService) 
     {
         if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
-        using var hmac = new HMACSHA512();
+        return Ok();
+
+        /* using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
@@ -32,7 +34,7 @@ public class AccountController(DataContext context, ITokenService tokenService) 
         {
             Username= user.UserName,
             Token = tokenService.CreateToken(user)
-        };
+        }; */
     }
 
     [HttpPost("login")]
---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using API.DTOs;
+using API.Entities;
+
+namespace API.Interface
+{
+    
+};
+
+public interface IUserRepository
+{
+    void Update(AppUser user);
+    Task<bool> SaveAllAsync();
+    Task<IEnumerable<AppUser>> GetUserAsync();
+    Task<AppUser?> GetUserByIdAsync(int id);
+    Task<AppUser?> GetUserByUsernameAsync(string username);
+
+    Task<IEnumerable<MemberDto>> GetMembersAsync();
+
+    Task<MemberDto?> GetMemberAsync(string username);
+}
